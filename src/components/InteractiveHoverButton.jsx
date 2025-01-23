498cf63e6cd25dd713308d8c2470b8850ee1247f
Arpit Yadav <arpityadav.exe@gmail.com>
2025-01-23 23:58:15 +0530

    added dashboard dummy in the landing page + minor stylings

--- src/components/InteractiveHoverButton.jsx
+++ src/components/InteractiveHoverButton.jsx
@@ -13,7 +13,7 @@ export function InteractiveHoverButton({ text = "Button", className }) {
         <span>{text}</span>
         <ArrowRight />
       </div>
-      <div className="absolute left-[15%] top-[40%] h-2 w-2 scale-[1] rounded-lg bg-black transition-all duration-300 group-hover:left-[0%] group-hover:top-[0%] group-hover:h-full group-hover:w-full group-hover:scale-[1.8] group-hover:bg-fuchsia-400 dark:group-hover:bg-fuchsia-600"></div>
+      <div className="absolute left-[15%] top-[40%] h-2 w-2 scale-[1] rounded-lg bg-black transition-all duration-300 group-hover:left-[0%] group-hover:top-[0%] group-hover:h-full group-hover:w-full group-hover:scale-[1.8] group-hover:bg-fuchsia-400 dark:group-hover:bg-[#BE02E9]"></div>
     </div>
   );
 }
